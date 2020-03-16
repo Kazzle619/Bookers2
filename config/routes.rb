f46f197d00415656bc6919a55426ce9ac89d1bbe
utkazu <hiroshimazeal@gmail.com>
2020-03-16 21:54:17 +0900

    [Update] 5-9 モデル、コントローラーの作成とルーティングの設定まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'homes#top'
+  get 'homes/about'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
 end
